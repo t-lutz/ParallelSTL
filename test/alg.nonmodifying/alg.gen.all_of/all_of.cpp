fb0048f477b18df3b5cf43f42b89db5604f98387
tibo <tibo@dhcppc2.(none)>
2014-01-29 19:05:48 +0000

    Integrated tests in CMakeLists

--- test/alg.nonmodifying/alg.gen.all_of/all_of.cpp
+++ test/alg.nonmodifying/alg.gen.all_of/all_of.cpp
@@ -7,13 +7,17 @@
 #define EXECUTION_POLICY std::seq
 #endif
 
-TEST(all_of, Basic) {
+TEST(all_of, True) {
   std::vector<int> v{1,2,3,4,5,6,7,8,9};
 
   // everything is less than 10
   EXPECT_TRUE(std::all_of(//EXECUTION_POLICY,
                           std::begin(v), std::end(v),
                           [](const int &i){ return i < 10; }));
+}
+
+TEST(all_of, False) {
+  std::vector<int> v{1,2,3,4,5,6,7,8,9};
 
   // not everything is 1
   EXPECT_FALSE(std::all_of(//EXECUTION_POLICY,
---
+++ test/alg.nonmodifying/alg.gen.none_of/none_of.cpp
@@ -0,0 +1,36 @@
+#include "gtest/gtest.h"
+
+#include <algorithm>
+#include <vector>
+
+#ifndef EXECUTION_POLICY
+#define EXECUTION_POLICY std::seq
+#endif
+
+TEST(none_of, True) {
+  std::vector<int> v{1,2,3,4,5,6,7,8,9};
+
+  // everything is less than 10
+  EXPECT_TRUE(std::none_of(//EXECUTION_POLICY,
+                           std::begin(v), std::end(v),
+                           [](const int &i){ return i == 10; }));
+}
+
+TEST(none_of, False) {
+  std::vector<int> v{1,2,3,4,5,6,7,8,9};
+
+  // not everything is 1
+  EXPECT_FALSE(std::none_of(//EXECUTION_POLICY,
+                            std::begin(v), std::end(v),
+                            [](const int &i){ return i == 1; }));
+}
+
+TEST(none_of, Empty) {
+  std::vector<int> v;
+  
+  // empty range should return true regardless of the predicate
+  EXPECT_TRUE(std::none_of(//EXECUTION_POLICY,
+                           std::begin(v), std::end(v),
+                           [](const int &i){ return false; }));
+}
+

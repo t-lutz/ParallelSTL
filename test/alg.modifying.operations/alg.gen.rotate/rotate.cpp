298de4eca538bf098d5e41c7960b109fd65a4869
Michel Steuwer <michel.steuwer@uni-muenster.de>
2014-02-26 15:38:47 +0000

    Added tests for rotate

--- test/alg.modifying.operations/alg.gen.rotate/rotate.cpp
+++ test/alg.modifying.operations/alg.gen.rotate/rotate.cpp
@@ -1,5 +1,80 @@
 #include "gtest/gtest.h"
 
-TEST(rotate, NotImplemented){
-   EXPECT_TRUE(false);
+#include <vector>
+
+#ifdef EXECUTION_POLICY
+#include <parallel/algorithm>
+#else
+#include <algorithm>
+#endif
+
+TEST(rotate, MoveToLast)
+{
+  using namespace std;
+  vector<int> v{1,2,3,4,5,6,7,8,9,10,11,12};
+  const vector<int> gold{2,3,4,5,6,7,8,9,10,11,12,1};
+
+  rotate(
+#ifdef EXECUTION_POLICY
+      EXECUTION_POLICY,
+#endif
+      begin(v), begin(v)+1, end(v));
+
+  EXPECT_EQ(gold, v);
+}
+
+TEST(rotate, MoveRangeToEnd)
+{
+  using namespace std;
+  vector<int> v{1,2,3,4,5,6,7,8,9,10,11,12};
+  const vector<int> gold{7,8,9,10,11,12,1,2,3,4,5,6};
+
+  auto middle = [](vector<int>& v) {
+    return begin(v) + v.size()/2;
+  };
+
+  rotate(
+#ifdef EXECUTION_POLICY
+      EXECUTION_POLICY,
+#endif
+      begin(v), middle(v), end(v));
+
+  EXPECT_EQ(gold, v);
+}
+
+TEST(rotate, MoveRangeToMiddle)
+{
+  using namespace std;
+  vector<int> v{1,2,3,4,5,6,7,8,9,10,11,12};
+  const vector<int> gold{4,5,6,1,2,3,7,8,9,10,11,12};
+
+  auto middle = [](vector<int>& v) {
+    return begin(v) + v.size()/2;
+  };
+
+  auto quarter = [](vector<int>& v) {
+    return begin(v) + v.size()/4;
+  };
+
+  rotate(
+#ifdef EXECUTION_POLICY
+      EXECUTION_POLICY,
+#endif
+      begin(v), quarter(v), middle(v));
+
+  EXPECT_EQ(gold, v);
+}
+
+TEST(rotate, Empty)
+{
+  using namespace std;
+  vector<int> v;
+
+  rotate(
+#ifdef EXECUTION_POLICY
+      EXECUTION_POLICY,
+#endif
+      begin(v), begin(v), end(v));
+
+  EXPECT_TRUE(v.empty());
 }
--- test/alg.modifying.operations/alg.gen.rotate/rotate_copy.cpp
+++ test/alg.modifying.operations/alg.gen.rotate/rotate_copy.cpp
@@ -1,5 +1,84 @@
 #include "gtest/gtest.h"
 
-TEST(rotate_copy, NotImplemented){
-   EXPECT_TRUE(false);
+#include <vector>
+
+#ifdef EXECUTION_POLICY
+#include <parallel/algorithm>
+#else
+#include <algorithm>
+#endif
+
+TEST(rotate_copy, MoveToLast)
+{
+  using namespace std;
+  const vector<int> v1{1,2,3,4,5,6,7,8,9,10,11,12};
+  vector<int> v2; v2.resize(v1.size());
+  const vector<int> gold{2,3,4,5,6,7,8,9,10,11,12,1};
+
+  rotate_copy(
+#ifdef EXECUTION_POLICY
+      EXECUTION_POLICY,
+#endif
+      begin(v1), begin(v1)+1, end(v1), begin(v2));
+
+  EXPECT_EQ(gold, v2);
+}
+
+TEST(rotate_copy, MoveRangeToEnd)
+{
+  using namespace std;
+  const vector<int> v1{1,2,3,4,5,6,7,8,9,10,11,12};
+  vector<int> v2; v2.resize(v1.size());
+  const vector<int> gold{7,8,9,10,11,12,1,2,3,4,5,6};
+
+  auto middle = [](const vector<int>& v) {
+    return begin(v) + v.size()/2;
+  };
+
+  rotate_copy(
+#ifdef EXECUTION_POLICY
+      EXECUTION_POLICY,
+#endif
+      begin(v1), middle(v1), end(v1), begin(v2));
+
+  EXPECT_EQ(gold, v2);
+}
+
+TEST(rotate_copy, MoveRangeToMiddle)
+{
+  using namespace std;
+  const vector<int> v1{1,2,3,4,5,6,7,8,9,10,11,12};
+  vector<int> v2; v2.resize(v1.size());
+  const vector<int> gold{4,5,6,1,2,3,0,0,0,0,0,0};
+
+  auto middle = [](const vector<int>& v) {
+    return begin(v) + v.size()/2;
+  };
+
+  auto quarter = [](const vector<int>& v) {
+    return begin(v) + v.size()/4;
+  };
+
+  rotate_copy(
+#ifdef EXECUTION_POLICY
+      EXECUTION_POLICY,
+#endif
+      begin(v1), quarter(v1), middle(v1), begin(v2));
+
+  EXPECT_EQ(gold, v2);
+}
+
+TEST(rotate_copy, Empty)
+{
+  using namespace std;
+  const vector<int> v1;
+  vector<int> v2;
+
+  rotate_copy(
+#ifdef EXECUTION_POLICY
+      EXECUTION_POLICY,
+#endif
+      begin(v1), begin(v1), end(v1), begin(v2));
+
+  EXPECT_TRUE(v2.empty());
 }

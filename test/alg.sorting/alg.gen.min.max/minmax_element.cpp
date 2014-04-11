1fcc289707b4dcd6a4f37cd2dd3a4eeb31e0598c
Thibaut Lutz <thibaut.lutz@gmail.com>
2014-04-12 00:22:21 +0100

    added test for minmax

--- test/alg.sorting/alg.gen.min.max/minmax_element.cpp
+++ test/alg.sorting/alg.gen.min.max/minmax_element.cpp
@@ -1,5 +1,201 @@
 #include "gtest/gtest.h"
 
-TEST(minmax_element, NotImplemented){
-   EXPECT_TRUE(false);
+#include <vector>
+
+#ifdef EXECUTION_POLICY
+#include <experimental/algorithm>
+#else
+#include <algorithm>
+#endif
+
+//template<class ExecutionPolicy,class ForwardIterator>
+//ForwardIterator minmax_element(ExecutionPolicy &&exec,
+//                            ForwardIterator first, ForwardIterator last);
+TEST(minmax_element, Unique) 
+{
+  using namespace std; 
+  vector<int> v{0,0,0,0,1,0,-1,0,0,0,0,0,0,0,0,0};
+
+  {
+    auto minmax = minmax_element(
+#ifdef EXECUTION_POLICY
+                                 EXECUTION_POLICY,
+#endif
+                                 begin(v), end(v));
+
+    EXPECT_EQ(6, distance(begin(v), minmax.first));
+    EXPECT_EQ(4, distance(begin(v), minmax.second));
+  }
+
+  v[2] = 2;
+  v[8] = -2;
+  {
+    auto minmax = minmax_element(
+#ifdef EXECUTION_POLICY
+                                 EXECUTION_POLICY,
+#endif
+                                 begin(v), end(v));
+
+    EXPECT_EQ(8, distance(begin(v), minmax.first));
+    EXPECT_EQ(2, distance(begin(v), minmax.second));
+
+  }
 }
+
+
+TEST(minmax_element, Multiple) 
+{
+  using namespace std;
+  vector<int> v{0,0,-1,0,1,1,1,1,1,1,-1,-1,0,0,0,0};
+
+  {
+    auto minmax = minmax_element(
+#ifdef EXECUTION_POLICY
+                                 EXECUTION_POLICY,
+#endif
+                                 begin(v), end(v));
+
+    EXPECT_EQ(2, distance(begin(v), minmax.first));
+    EXPECT_EQ(9, distance(begin(v), minmax.second));
+  }
+
+  v[2] = v[4] = 2;
+  v[1] = v[5] = -2;
+  {
+    auto minmax = minmax_element(
+#ifdef EXECUTION_POLICY
+                                 EXECUTION_POLICY,
+#endif
+                                 begin(v), end(v));
+
+    
+    EXPECT_EQ(1, distance(begin(v), minmax.first));
+    EXPECT_EQ(4, distance(begin(v), minmax.second));
+
+  }
+}
+
+TEST(minmax_element, AllSame) 
+{
+  using namespace std;
+  vector<int> v{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+  auto minmax = minmax_element(
+#ifdef EXECUTION_POLICY
+                               EXECUTION_POLICY,
+#endif
+                               begin(v), end(v));
+
+  EXPECT_EQ(minmax.first, begin(v)); 
+  EXPECT_EQ(v.size()-1, distance(begin(v), minmax.second));
+}
+
+
+
+TEST(minmax_element, Empty){
+  std::vector<int> v;
+
+  auto minmax = std::minmax_element(
+#ifdef EXECUTION_POLICY
+                              EXECUTION_POLICY,
+#endif
+                              std::begin(v), std::end(v));
+  EXPECT_TRUE(minmax.first == end(v));
+  EXPECT_TRUE(minmax.second == end(v));
+}
+
+//template<class ExecutionPolicy, class ForwardIterator, class Compare>
+//ForwardIterator minmax_element(ExecutionPolicy &&exec,
+//                            ForwardIterator first, ForwardIterator last, Compare comp);
+TEST(minmax_element, UniqueComp) 
+{
+  using namespace std; 
+  vector<int> v{0,0,0,0,1,0,-1,0,0,0,0,0,0,0,0,0};
+
+  {
+    auto minmax = minmax_element(
+#ifdef EXECUTION_POLICY
+                                 EXECUTION_POLICY,
+#endif
+                                 begin(v), end(v), std::greater<int>());
+
+    EXPECT_EQ(4, distance(begin(v), minmax.first));
+    EXPECT_EQ(6, distance(begin(v), minmax.second));
+  }
+
+  v[2] = 2;
+  v[8] = -2;
+  {
+    auto minmax = minmax_element(
+#ifdef EXECUTION_POLICY
+                                 EXECUTION_POLICY,
+#endif
+                                 begin(v), end(v), std::greater<int>());
+
+    EXPECT_EQ(2, distance(begin(v), minmax.first));
+    EXPECT_EQ(8, distance(begin(v), minmax.second));
+
+  }
+}
+
+
+TEST(minmax_element, MultipleComp) 
+{
+  using namespace std;
+  vector<int> v{0,0,-1,0,1,1,1,1,1,1,-1,-1,0,0,0,0};
+
+  {
+    auto minmax = minmax_element(
+#ifdef EXECUTION_POLICY
+                                 EXECUTION_POLICY,
+#endif
+                                 begin(v), end(v), std::greater<int>());
+
+    EXPECT_EQ(4, distance(begin(v), minmax.first));
+    EXPECT_EQ(11, distance(begin(v), minmax.second));
+  }
+
+  v[2] = v[3] = 2;
+  v[1] = v[5] = -2;
+  {
+    auto minmax = minmax_element(
+#ifdef EXECUTION_POLICY
+                                 EXECUTION_POLICY,
+#endif
+                                 begin(v), end(v), std::greater<int>());
+
+    
+    EXPECT_EQ(2, distance(begin(v), minmax.first));
+    EXPECT_EQ(5, distance(begin(v), minmax.second));
+
+  }
+}
+
+TEST(minmax_element, AllSameComp) 
+{
+  using namespace std;
+  vector<int> v{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+  auto minmax = minmax_element(
+#ifdef EXECUTION_POLICY
+                               EXECUTION_POLICY,
+#endif
+                               begin(v), end(v), std::greater<int>());
+
+  EXPECT_EQ(minmax.first, begin(v)); 
+  EXPECT_EQ(v.size()-1, distance(begin(v), minmax.second));
+}
+
+
+
+TEST(minmax_element, EmptyComp){
+  std::vector<int> v;
+
+  auto minmax = std::minmax_element(
+#ifdef EXECUTION_POLICY
+                              EXECUTION_POLICY,
+#endif
+                              std::begin(v), std::end(v), std::greater<int>());
+  EXPECT_TRUE(minmax.first == end(v));
+  EXPECT_TRUE(minmax.second == end(v));
+}
+
+

b85b5ee3d92054802e5c8c1e221a1ef88169a644
Thibaut Lutz <thibaut.lutz@gmail.com>
2014-04-16 18:21:53 +0100

    Added a reduction example

---
+++ examples/sum_reduction.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <experimental/utility>
+
+// This should be converted to a dynamic policy when they are implemented
+template<typename T>
+auto sum_reduction(T && policy, const std::vector<double> & values)
+  -> std::chrono::microseconds
+{
+  using namespace std;
+
+  auto start = chrono::high_resolution_clock::now();
+  auto sum = reduce(policy, begin(values), end(values));
+  auto finish = chrono::high_resolution_clock::now();
+
+  cout << "sum = " << sum << endl;
+
+  return chrono::duration_cast<chrono::microseconds>(finish-start); 
+}
+
+int main()
+{
+  using namespace std;
+
+  vector<double> values(5000000);
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_real_distribution<> d(1, 2);
+
+  generate(begin(values), end(values), [&](){ return d(gen); });
+
+  cout << "Using seq: " 
+       << sum_reduction(experimental::parallel::seq, values).count() 
+       << " us" << endl;
+
+  cout << "Using par: " 
+       << sum_reduction(experimental::parallel::par, values).count() 
+       << " us" << endl;
+}
+
+

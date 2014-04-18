3a99d287fa13b6c2f93648707bc1d6c7cee89107
Thibaut Lutz <thibaut.lutz@gmail.com>
2014-04-18 15:22:57 +0100

    Implemented two more test cases

--- test/alg.modifying.operations/alg.gen.partitions/is_partitioned.cpp
+++ test/alg.modifying.operations/alg.gen.partitions/is_partitioned.cpp
@@ -1,5 +1,51 @@
 #include "gtest/gtest.h"
 
-TEST(is_partitioned, NotImplemented){
-   EXPECT_TRUE(false);
+#include <vector>
+
+#ifdef EXECUTION_POLICY
+#include <experimental/algorithm>
+#else
+#include <algorithm>
+#endif
+
+TEST(is_partitioned, Patitioned){
+  using namespace std;
+
+  const vector<int> v{1,3,5,7,9,11,2,4,6,8,10,12};
+
+  EXPECT_TRUE(is_partitioned(
+#ifdef EXECUTION_POLICY
+                             EXECUTION_POLICY,
+#endif
+                             begin(v), end(v),
+                             [](const int &i){ return i % 2 == 1;}));
 }
+
+TEST(is_partitioned, NotPatitioned){
+  using namespace std;
+
+  const vector<int> v{1,2,3,4,5,6,7,8,9,10,11,12};
+
+  EXPECT_FALSE(is_partitioned(
+#ifdef EXECUTION_POLICY
+                              EXECUTION_POLICY,
+#endif
+                              begin(v), end(v),
+                              [](const int &i){ return i % 2 == 1;}));
+}
+
+TEST(is_partitioned, Empty){
+  using namespace std;
+
+  const vector<int> v;
+
+  EXPECT_TRUE(is_partitioned(
+#ifdef EXECUTION_POLICY
+                             EXECUTION_POLICY,
+#endif
+                             begin(v), end(v),
+                             [](const int &i){ return i % 2 == 1;}));
+}
+
+
+
--- test/alg.sorting/alg.gen.sort/stable.sort/stable_sort.cpp
+++ test/alg.sorting/alg.gen.sort/stable.sort/stable_sort.cpp
@@ -1,5 +1,94 @@
 #include "gtest/gtest.h"
 
-TEST(stable_sort, NotImplemented){
-   EXPECT_TRUE(false);
+#include <vector>
+
+#ifdef EXECUTION_POLICY
+#include <experimental/algorithm>
+#else
+#include <algorithm>
+#endif
+
+TEST(stable_sort, PlainSort){
+  using namespace std;
+
+  vector<int> v{1,4,8,7,2,1,5,4,6,5,7,2,5,1,0};
+
+  stable_sort(
+#ifdef EXECUTION_POLICY
+              EXECUTION_POLICY,
+#endif
+              begin(v), end(v));
+
+  EXPECT_TRUE(is_sorted(begin(v), end(v)));
 }
+
+TEST(stable_sort, Sorted){
+  using namespace std;
+
+  vector<int> v{1,2,3,4,5,6,7,8,9,10};
+
+  stable_sort(
+#ifdef EXECUTION_POLICY
+              EXECUTION_POLICY,
+#endif
+              begin(v), end(v));
+
+  EXPECT_TRUE(is_sorted(begin(v), end(v)));
+}
+
+
+TEST(stable_sort, Empty){
+  using namespace std;
+
+  vector<int> v;
+
+  stable_sort(
+#ifdef EXECUTION_POLICY
+              EXECUTION_POLICY,
+#endif
+              begin(v), end(v));
+
+  EXPECT_TRUE(is_sorted(begin(v), end(v)));
+}
+
+TEST(stable_sort, StableSortComp){
+  using namespace std;
+
+  vector<double> v{9.0, 8.5, 8.0, 7.75, 7.5, 7.25, 7.0, 6.5, 6.0, 5.5, 5.0};
+
+  stable_sort(
+#ifdef EXECUTION_POLICY
+              EXECUTION_POLICY,
+#endif
+              begin(v), end(v),
+              [](const double &a, const double &b){
+                 return (int)a < (int)b;
+              });
+
+  EXPECT_TRUE(is_sorted(begin(v), end(v),
+              [](const double &a, const double &b) {
+                return (int)a < (int)b || ((int)a == (int)b && a > b);
+              }));
+}
+
+TEST(stable_sort, Sortable){
+  using namespace std;
+
+  struct Sortable {
+    int payload;
+    bool operator < (const Sortable &other) const {
+      return payload > other.payload;
+    }
+  };
+
+  vector<Sortable> v{{1}, {3}, {2}, {6}, {4}, {5}};
+
+  stable_sort(
+#ifdef EXECUTION_POLICY
+              EXECUTION_POLICY,
+#endif
+              begin(v), end(v));
+
+  EXPECT_TRUE(is_sorted(begin(v), end(v)));
+}
+

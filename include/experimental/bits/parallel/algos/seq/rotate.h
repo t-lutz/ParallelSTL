d4314a0be2f83936d74961a5ffb374dcc0ce1957
Thibaut Lutz <thibaut.lutz@gmail.com>
2014-04-30 18:36:23 +0100

    Used std::advance and std::distance to calculate the output of rotate without random access iterators. This is not an optimal solution. Fixes #2.

--- include/experimental/bits/parallel/algos/seq/rotate.h
+++ include/experimental/bits/parallel/algos/seq/rotate.h
@@ -13,9 +13,9 @@ namespace parallel {
       sequential_execution_policy::rotate(ForwardIterator first, ForwardIterator middle,
                   ForwardIterator last) const {
     std::rotate(first, middle, last);
-    // only fine for random access operators
-    // TODO: extend for generic forward iterators
-    return first + (last - middle);
+    // TODO: this is not optimal.
+    advance(first, distance(middle, last));
+    return first;
   }
 
 } // namespace parallel

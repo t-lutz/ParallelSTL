48d5b0cec7ba05fdc0ae689848c109f05677cf36
Marius Umlauf <post@m-umlauf.de>
2015-12-17 17:10:06 +0100

    bugfixed and improved the unit-test for fill*

--- test/alg.modifying.operations/alg.gen.fill/fill_n.cpp
+++ test/alg.modifying.operations/alg.gen.fill/fill_n.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 
 #ifdef EXECUTION_POLICY
-#include <experimental/algorithm>
+#include <experimental/parallel/algorithm>
 #else
 #include <algorithm>
 #endif
@@ -14,19 +14,45 @@ TEST(fill_n, Lambda){
   using namespace std::experimental;
 #endif
   
-  vector<int> v(100);
+  vector<int> v(10000);
 
-  fill_n(
+  auto it = fill_n(
 #ifdef EXECUTION_POLICY
               EXECUTION_POLICY,
 #endif
               begin(v), v.size(),
               1);
 
-  EXPECT_EQ(100, v.size());
   for(auto i : v){
-    EXPECT_EQ(1, i);
+    EXPECT_EQ(1, v[i]);
   }
+  EXPECT_EQ(end(v),it);
+}
+
+TEST(fill_n, partial){
+  using namespace std;
+#ifdef EXECUTION_POLICY
+  using namespace std::experimental;
+#endif
+
+  vector<int> v(10000);
+  v[8000]=23;
+  v[9000]=42;
+
+  auto it = fill_n(
+#ifdef EXECUTION_POLICY
+            EXECUTION_POLICY,
+#endif
+            begin(v), 8000,
+            123);
+
+  EXPECT_EQ(10000, v.size());
+  for(auto i=0; i < 8000; i++){
+    EXPECT_EQ(123, v[i]);
+  }
+  EXPECT_EQ(next(begin(v),8000),it);
+  EXPECT_EQ(42, v[9000]);
+  EXPECT_EQ(23, v[8000]);
 }
 
 
@@ -38,7 +64,7 @@ TEST(fill_n, Empty){
   
   vector<int> v;
 
-  fill_n(
+  auto it = fill_n(
 #ifdef EXECUTION_POLICY
                   EXECUTION_POLICY,
 #endif
@@ -46,6 +72,7 @@ TEST(fill_n, Empty){
                   1);
 
   EXPECT_TRUE(v.empty());
+  EXPECT_EQ(begin(v),it);
 }
 
 
@@ -57,16 +84,16 @@ TEST(fill_n, Negative){
   
   vector<int> v(100);
 
-  fill_n(
+  auto it = fill_n(
 #ifdef EXECUTION_POLICY
                   EXECUTION_POLICY,
 #endif
                   begin(v), -100,
                   1);
 
-  EXPECT_EQ(100, v.size());
+  EXPECT_EQ(begin(v),it);
   for(auto i : v){
-    EXPECT_EQ(0, i);
+    EXPECT_EQ(0, v[i]);
   }
 }
 
